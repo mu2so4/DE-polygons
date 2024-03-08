89aeb75a1502d6c6f03b52ad7c9c380947c82b1f
mu2so4 <b4.orangutang@gmail.com>
2024-03-08 13:18:41 +0700

    Update output format

--- main.py
+++ main.py
@@ -17,23 +17,32 @@ polygon1 = read_polygon(filename1)
 
 filename2 = sys.argv[2]
 polygon2 = read_polygon(filename2)
+gpd.GeoSeries([polygon1, polygon2]).boundary.plot()
+plt.savefig("initial_position.png")
 
 mergedPolys = polygon1.union(polygon2)
 gpd.GeoSeries([mergedPolys]).plot()
 plt.savefig("union.png")
+print("Union")
 print(mergedPolys)
+print()
 
 intersectedPolys = polygon1.intersection(polygon2)
 gpd.GeoSeries([intersectedPolys]).plot()
 plt.savefig("intersection.png")
+print("Intersection")
 print(intersectedPolys)
+print()
 
 diff_1_2 = polygon1.difference(polygon2)
 gpd.GeoSeries([diff_1_2]).plot()
 plt.savefig("difference_1_2.png")
+print("Difference 1 without 2")
 print(diff_1_2)
+print()
 
 diff_2_1 = polygon2.difference(polygon1)
 gpd.GeoSeries([diff_2_1]).plot()
 plt.savefig("difference_2_1.png")
+print("Difference 2 without 1")
 print(diff_2_1)
