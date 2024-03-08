210d691ee70d3372a42bc5308389e55d6f47ba6f
mu2so4 <b4.orangutang@gmail.com>
2024-03-08 12:16:15 +0700

    Add union of two polygons

--- main.py
+++ main.py
@@ -1,26 +1,36 @@
 import sys
 import matplotlib.pyplot as plt
+from shapely.geometry import Polygon
+from shapely.ops import unary_union
+import geopandas as gpd
 
-def read_polynom(filename):
+def read_polygon(filename):
 	with open(filename) as inp_file:
 		data = inp_file.readlines()
 		coords = []
 		for line in data:
 			str_numbers = line.split()
 			coords.append([float(s) for s in str_numbers])
+	'''
 	coords.append(coords[0])
 	return coords
+	'''
+	return Polygon(coords)
 
 filename1 = sys.argv[1]
-polynom1 = read_polynom(filename1)
-xs1, ys1 = zip(*polynom1)
+polygon1 = read_polygon(filename1)
+#xs1, ys1 = zip(*polygon1)
 #plt.figure()
-plt.plot(xs1, ys1)
+#plt.plot(xs1, ys1)
 
 filename2 = sys.argv[2]
-polynom2 = read_polynom(filename2)
-xs2, ys2 = zip(*polynom2)
+polygon2 = read_polygon(filename2)
+#xs2, ys2 = zip(*polygon2)
 #plt.figure()
-plt.plot(xs2, ys2)
+#plt.plot(xs2, ys2)
+polys = [polygon1, polygon2]
 
+mergedPolys = unary_union(polys)
+gpd.GeoSeries([mergedPolys]).boundary.plot()
 plt.show()
+
